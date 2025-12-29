8500af472b5ffda2db7740b216047c51e5875f38
kacastilloch <kacastillo.ch@gmail.com>
2025-12-29 18:51:20 -0300

    primer commit del proyecto

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+let a = 5, b = 10;
+let c = a + b;
+
+console.log("resultado", c);
+
+//enlazar div del html
+let bloque = document.getElementById("bloque");
+
+//agregar un parrafo con texto al div 
+
+bloque.innerHTML = "<p>un parrafo agregado con javascript :D</p>"
